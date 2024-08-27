473765904552b2e081678bc85abea2ac9c04e788
ux-engineer <info@envision.fi>
2024-08-27 16:53:41 +0300

    First release version 1.0.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
@@ -7,11 +8,16 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      // Disable XL & 2XL responsive breakpoints
+      screens: {
+        'xl': '1024px',
+        '2xl': '1024px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['prompt', ...defaultTheme.fontFamily.sans],
       },
     },
   },
